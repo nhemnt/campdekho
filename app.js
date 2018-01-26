aeeea45e463ac445a5b3632fe1805e05c0bd7c2d
hemant <nhemnt3@gmail.com>
2018-01-26 14:21:00 +0000

    mongoose connect -update to databaseurl

--- app.js
+++ app.js
@@ -15,9 +15,13 @@ var express     = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
+   
     
+mongoose.connect(process.env.DATABASEURL);
 // mongoose.connect("mongodb://localhost/yelp_camp_v10");
-mongoose.connect("mongodb://nhemnt:password@ds127998.mlab.com:27998/yelpcamp");
+// mongoose.connect("mongodb://nhemnt:password@ds127998.mlab.com:27998/yelpcamp");
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
